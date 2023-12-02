45bba5abfa5f66fcc67e21c3dbfc32cf4d62cf32
DKowalski25 <dshinoda25@gmail.com>
2023-12-02 17:15:00 +0700

    The final assembly of the book_bot

---
+++ filters/filters.py
@@ -0,0 +1,15 @@
+from aiogram.filters import BaseFilter
+from aiogram.types import CallbackQuery
+
+
+class IsDigitalCallbackData(BaseFilter):
+    async def __call__(self, callback: CallbackQuery) -> bool:
+        return callback.data.isdigit()
+
+
+class IsDelBookmarkCallbackDate(BaseFilter):
+    async def __call__(self, callback: CallbackQuery) -> bool:
+        return callback.data.endswith('del') and callback.data[:-3].isdigit()
+
+
+
